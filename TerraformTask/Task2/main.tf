6d233ca5199523b3f3d853cebfbc09afce86f862
ahmedashraf1907 <ashrsafahmed@gmail.com>
2026-06-15 19:51:45 +0300

    Remote-exec

---
+++ TerraformTask/Task2/main.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+}
+resource "aws_instance" "ec2" {
+  subnet_id = aws_subnet.my_subnet.id
+  ami = "ami-0fdfb4d987b63ae72"
+  instance_type = "t3.micro"
+  associate_public_ip_address = true
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("mykey.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    script = "./apache.sh"
+  }
+}
